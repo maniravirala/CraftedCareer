bbc2cf578849b023b9f129b01e3228ec532d0b71
Mani Ravirala <raviralamani5@gmail.com>
2024-04-30 20:05:32 +0530

    feat: Add ad link on Test page

--- src/app/Test/Test.jsx
+++ src/app/Test/Test.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import Lottie from 'react-lottie';
-import { arrowLottie } from '../../assets';
 
 const Test = () => {
   return (
     <div>
-      <Lottie
-        options={{
-          loop: true,
-          autoplay: true,
-          animationData: arrowLottie,
-          rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice',
-          },
-        }}
-        height={400}
-        width={400}
-      />
+      <h1>Test</h1>
+      <a target='_blank' href="https://www.highcpmgate.com/ktzrpcar?key=a1544f3308d0670e5346d54cfedcb711"
+      className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+      >Click here to see ad for 5 seconds</a>
+      
     </div>
 
   );
