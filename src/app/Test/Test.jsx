ae131a9ead151ce6497c329e6c7482018de97dd6
Mani Ravirala <raviralamani5@gmail.com>
2024-04-30 20:32:34 +0530

    Refactor: Add additional links to Test component

--- src/app/Test/Test.jsx
+++ src/app/Test/Test.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
 const Test = () => {
+  
+  const link1 = 'https://www.highcpmgate.com/ktzrpcar?key=a1544f3308d0670e5346d54cfedcb711';
+  const link2 = 'https://www.highcpmgate.com/k47vsuqswz?key=34301e33e2c786f34e6d18031a7aea6c';
+  const link3 = 'https://glaultoa.com/4/7416741';
+  const link4 = 'https://eephoawaum.com/4/7416872';
+
   return (
     <div>
       <h1>Test</h1>
-      {/* https://www.highcpmgate.com/ktzrpcar?key=a1544f3308d0670e5346d54cfedcb711 create a btn with this link */}
-      <a href="https://www.highcpmgate.com/ktzrpcar?key=a1544f3308d0670e5346d54cfedcb711" target="_blank" rel="noreferrer">Click here</a>
       
+      <a href={link1} target="_blank" rel="noreferrer">Link 1</a>
+      <br />
+      <a href={link2} target="_blank" rel="noreferrer">Link 2</a>
+      <br />
+      <a href={link3} target="_blank" rel="noreferrer">Link 3</a>
+      <br />
+      <a href={link4} target="_blank" rel="noreferrer">Link 4</a>
+
     </div>
 
   );
